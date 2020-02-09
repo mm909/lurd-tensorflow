dc3f279d6ce8f8ac48c7fab81cec436d6f6f348e
Mikian Musser <Bobar312@gmail.com>
2020-02-09 15:59:09 -0800

    Attempting cnn for lurd

--- cnnlurd.py
+++ cnnlurd.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from progress.bar import Bar
+import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -16,7 +17,7 @@ samples = 9452
 # featureCount = 2000
 # samples = 10983
 
-split = 0.90
+split = 0.95
 splitNum = int(samples * split)
 
 print("Opening and reading data file...")
@@ -43,26 +44,31 @@ with open(file) as lurdFile:
             bar.next()
             pass
 
-trainx = features[:splitNum]
-trainy = labels[:splitNum]
-testx = features[splitNum:]
-testy = labels[splitNum:]
+trainx = np.expand_dims(np.array(features[:splitNum]), axis = 2)
+trainy = np.expand_dims(np.array(labels[:splitNum]), axis = 1)
+testx  = np.expand_dims(np.array(features[splitNum:]), axis = 2)
+testy  = np.expand_dims(np.array(labels[splitNum:]), axis = 1)
+
+print(trainx.shape)
+print(trainy.shape)
+print(testx.shape)
+print(testy.shape)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(featureCount,)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(4, activation='softmax')
+    tf.keras.layers.Conv1D(128, kernel_size=3, activation='relu', input_shape=( 2000, 1)),
+    tf.keras.layers.MaxPooling1D(pool_size=2),
+    tf.keras.layers.Conv1D(64, kernel_size=3, activation='relu'),
+    tf.keras.layers.MaxPooling1D(pool_size=2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(4, activation='softmax'),
 ])
+
+model.summary()
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
 
 model.fit(trainx, trainy, epochs = 10)
 model.evaluate(testx,  testy, verbose = 2)
